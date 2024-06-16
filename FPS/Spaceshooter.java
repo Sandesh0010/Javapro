24ea5614331e14ae4b1912f9bc9eb99937277269
Sandesh0010 <108848228+Sandesh0010@users.noreply.github.com>
2024-06-16 09:35:27 +0545

    Add files via upload
    changes

---
+++ FPS/Spaceshooter.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+class Spaceshooter 
+{
+    public static void main(String arg[])
+    { 
+        JFrame f=new JFrame();
+        f.setResizable(false);//to make unresizble
+        Gamepanel panel =new Gamepanel();
+        f.add(panel);
+        panel.startGame();
+        //  f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(500,500);
+        f.setVisible(true);
+    }
+
+}
