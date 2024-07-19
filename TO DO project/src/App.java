c704bd9a877a8e6b907950ec12b38754a4bb0668
Sandesh0010 <108848228+Sandesh0010@users.noreply.github.com>
2024-07-19 10:23:28 +0545

    connected to database and added data in the table

--- TO DO project/src/App.java
+++ TO DO project/src/App.java
@@ -2,25 +2,56 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class App {
+    static Connection connection;
+    static JFrame frame;
+    static JTextField textField;
+    static JButton addButton;
+    static DefaultTableModel tableModel;
     public static void main(String[] args) throws Exception {
-        JFrame frame= new JFrame("MY TO DO");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url="jdbc:mysql://localhost:3306/my-tod";
+        String username="root";
+        String password="root";
+        connection=DriverManager.getConnection(url,username,password);
+        System.out.println("connected");
+
+
+        frame= new JFrame("MY TO DO");
        frame.setSize(500, 500);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout());
         
-        JTextField textField=new JTextField(20);
-        JButton addButton = new JButton("Add");
+        textField=new JTextField(20);
+        addButton = new JButton("Add");
         JPanel panel=new JPanel();
         panel.add(textField);
         panel.add(addButton);
         frame.add(panel,BorderLayout.NORTH);
 
-        DefaultTableModel tableModel= new DefaultTableModel(new Object[]
+        tableModel= new DefaultTableModel(new Object[]
         {"ID","Task","Is Completed"},0);
         JTable table= new JTable(tableModel);
         JScrollPane scroll = new JScrollPane(table);
         frame.add (scroll,BorderLayout.SOUTH);
+        loadData();
+
+
+
+        //String query ="SELECT * FROM datas";
+        //Statement statement= connection.createStatement();
+        //statement.executeQuery(query);
+        //ResultSet rows=statement.executeQuery(query);
+        //while (rows.next()){
+           // tableModel.addRow(new Object[]{rows.getInt("ID"), rows.getString("Task"),rows.getString("completed")});
+
+        //}
+
 
         // tableModel.addRow(new Object[]{1,"Test","Completed"});
         // tableModel.addRow(new Object[]{2,"Test","Completed"});
@@ -28,4 +59,22 @@ public class App {
         //all code here before set Visible
         frame.setVisible(true);
     }
+
+
+private static void loadData(){
+    String query ="SELECT * FROM datas";
+    Statement statement;
+    try{
+         statement= connection.createStatement();
+        //statement.executeQuery(query);
+        ResultSet rows=statement.executeQuery(query);
+        while (rows.next()){
+            tableModel.addRow(new Object[]{rows.getInt("ID"), rows.getString("Task"),rows.getString("completed")});
+        }
+    }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    
+}
 }
